27fcadc9621cceb939dc255ea44d68af4061e484
Bobzikot <dirolhoper5@gmail.com>
2024-07-15 19:14:39 +0300

    fling and people (roblox) teleport.lua

--- fling and people (roblox) teleport.lua
+++ fling and people (roblox) teleport.lua
@@ -1,34 +1,37 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
-local Window = Libarary.CreateLib("Teleporting", "RJTheme1")
 
-local Tab = Window:NewTab ("teleport")
+local Window = Library.CreateLib("Teleporting", "RJTheme1")
+
+local Tab = Window:NewTab("Teleport")
 
 local Section = Tab:NewSection("Teleport")
 
-Section:NewKeybind("Teleport", "KeybindInfo", Enum.KeyCode.Z, fuction()
+local function TeleportToPlayers()
     local players = game:GetService("Players"):GetPlayers()
-local localCharacter = game.Players.LocalPlayer.Character
-local visitedPlayers = {}
-local hasTeleported = false
-local previousCFrame = localCharacter.HumanoidRootPart.CFrame
+    local localCharacter = game.Players.LocalPlayer.Character
+    local visitedPlayers = {}
+    local hasTeleported = false
+    local previousCFrame = localCharacter.HumanoidRootPart.CFrame
 
-while true do
-    if hasTeleported then
-        break
-    end
-    for _, player in pairs(players) do
-        if player ~= game.Players.LocalPlayer and not visitedPlayers[player] then
-            if player.Character then
-                previousCFrame = localCharacter.HumanoidRootPart.CFrame
-                localCharacter.HumanoidRootPart.CFrame = player.Character.HumanoidRootPart.CFrame
-                wait(0.001) -- extremely fast teleportation
-                localCharacter.HumanoidRootPart.CFrame = previousCFrame
-                visitedPlayers[player] = true
+    while true do
+        if hasTeleported then
+            break
+        end
+        for _, player in pairs(players) do
+            if player ~= game.Players.LocalPlayer and not visitedPlayers[player] then
+                if player.Character then
+                    previousCFrame = localCharacter.HumanoidRootPart.CFrame
+                    localCharacter.HumanoidRootPart.CFrame = player.Character.HumanoidRootPart.CFrame
+                    wait(0.001) -- extremely fast teleportation
+                    localCharacter.HumanoidRootPart.CFrame = previousCFrame
+                    visitedPlayers[player] = true
+                end
             end
         end
+        if table.Count(visitedPlayers) == #players - 1 then
+            hasTeleported = true
+        end
     end
-    if table.Count(visitedPlayers) == #players - 1 then
-        hasTeleported = true
-    end
-end 
-end)
+end
+
+Section:NewKeybind("Teleport to Players", "Teleport to all players", Enum.KeyCode.Z, TeleportToPlayers)
