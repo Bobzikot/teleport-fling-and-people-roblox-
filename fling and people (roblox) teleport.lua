949026e72ca23d351861a9e8defdfba113af790d
Bobzikot <dirolhoper5@gmail.com>
2024-07-21 23:17:21 +0300

    Update fling and people (roblox) teleport.lua

--- fling and people (roblox) teleport.lua
+++ fling and people (roblox) teleport.lua
@@ -4,32 +4,32 @@ local Tab = Window:NewTab("Teleport")
 local Section = Tab:NewSection("Teleport")
 local toggle = true
 local function TeleportToPlayers()
-    if toggle then
+if toggle then
 local players = game:GetService("Players"):GetPlayers()
 local localCharacter = game.Players.LocalPlayer.Character
 local visitedPlayers = {}
 local hasTeleported = false
 local previousCFrame = localCharacter.HumanoidRootPart.CFrame
 local originalCFrame = localCharacter.HumanoidRootPart.CFrame
-   while true do
-         if hasTeleported then
-         break
-      end
+while true do
+if hasTeleported then
+      break
+  end
  for _, player in pairs(players) do
-         if player ~= game.Players.LocalPlayer and not visitedPlayers[player] then
-            if player.Character then
+if player ~= game.Players.LocalPlayer and not visitedPlayers[player] then
+if player.Character then
 previousCFrame = localCharacter.HumanoidRootPart.CFrame
 localCharacter.HumanoidRootPart.CFrame = player.Character.HumanoidRootPart.CFrame
 wait(0.001) 
 localCharacter.HumanoidRootPart.CFrame = previousCFrame
 visitedPlayers[player] = true
-      end
-         end                  
-             end
-   if table.Count(visitedPlayers) == #players - 1 then
-hasTeleported = true
+    end
+        end                  
             end
+if table.Count(visitedPlayers) == #players - 1 then
+hasTeleported = true
         end
+    end
 localCharacter.HumanoidRootPart.CFrame = originalCFrame
     end
 end
